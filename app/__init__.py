9b8115b04a6122384b54497014707bac95b34279
Libby L. Gerber <libby@libbygerber.com>
2021-04-30 13:53:35 -0700

    finished the project

--- app/__init__.py
+++ app/__init__.py
@@ -9,14 +9,15 @@ migrate = Migrate()
 def create_app(test_config=None):
     app = Flask(__name__)
 
-    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
 
-    # db.init_app(app)
-    # migrate.init_app(app, db)
-    # from app.models.book import Book
+    db.init_app(app)
+    migrate.init_app(app, db)
+    from app.models.planet import Planet
 
+    # Register Blueprints here
     from .routes import planets_bp
     app.register_blueprint(planets_bp)
-    
+
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from app import db
-# from app.models.planet import Planet
+from app.models.planet import Planet
 from flask import request, Blueprint, make_response, jsonify
 
 planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
@@ -7,37 +7,33 @@ planets_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
 @planets_bp.route("", methods=["GET", "POST"])
 def handle_planets():
-    return "hello"
+    if request.method == "GET":
+        planets = Planet.query.all()
+        planets_response = []
+        for planet in planets:
+            planets_response.append({
+                "id": planet.id,
+                "name": planet.name,
+                "description": planet.description
+            })
+        return jsonify(planets_response)
+    elif request.method == "POST":
+        request_body = request.get_json()
+        new_planet = Planet(name=request_body["name"],
+                        description=request_body["description"])
+
+        db.session.add(new_planet)
+        db.session.commit()
+
+        return make_response(f"Planet {new_planet.name} successfully created", 201)
 
 
-# def handle_books():
-#     if request.method == "GET":
-#         books = Book.query.all()
-#         books_response = []
-#         for book in books:
-#             books_response.append({
-#                 "id": book.id,
-#                 "title": book.title,
-#                 "description": book.description
-#             })
-#         return jsonify(books_response)
-#     elif request.method == "POST":
-#         request_body = request.get_json()
-#         new_book = Book(title=request_body["title"],
-#                         description=request_body["description"])
+@planets_bp.route("/<planet_id>", methods=["GET"])
+def handle_planet(planet_id):
+    planet = Planet.query.get(planet_id)
 
-#         db.session.add(new_book)
-#         db.session.commit()
-
-#         return make_response(f"Book {new_book.title} successfully created", 201)
-
-
-# @books_bp.route("/<book_id>", methods=["GET"])
-# def handle_book(book_id):
-#     book = Book.query.get(book_id)
-
-#     return {
-#         "id": book.id,
-#         "title": book.title,
-#         "description": book.description
-#     }
+    return {
+        "id": planet.id,
+        "name": planet.name,
+        "description": planet.description
+    }
