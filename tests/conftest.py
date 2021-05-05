84fa41d0793d3cdbd85f5b85ad37d332c530a9dc
Libby L. Gerber <libby@libbygerber.com>
2021-05-05 07:22:36 -0700

    created tests

---
+++ tests/conftest.py
@@ -0,0 +1,30 @@
+import pytest
+from app import create_app
+from app import db
+from app.models.planet import Planet
+
+@pytest.fixture
+def app():
+    app = create_app({"TESTING": True})
+
+    with app.app_context():
+        db.create_all()
+        yield app
+
+    with app.app_context():
+        db.drop_all()
+
+
+@pytest.fixture
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture
+def two_saved_planets(app):
+    # Arrange
+    jupiter = Planet(name="Jupiter", description="cool planet")
+    mars = Planet(name="Mars", description="red planet")
+
+    db.session.add_all([jupiter, mars])
+    db.session.commit()
